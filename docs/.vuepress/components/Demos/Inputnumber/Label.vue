223b115c43484fd2c9d33d806e31b8243dff0caf
Marcel Gregoriadis <marcel.g98@live.de>
2021-02-20 14:30:21 +0100

    deploy

--- docs/.vuepress/components/Demos/Inputnumber/Label.vue
+++ docs/.vuepress/components/Demos/Inputnumber/Label.vue
@@ -1,15 +1,17 @@
 <template lang="html">
   <div class="centerx">
-    <vs-input-number v-model="number0" label="passengers:"/>
+    <vs-input-number v-model="number0" label="passengers:" />
+    <vs-input-number v-model="number1" label-right="kg" />
   </div>
 </template>
 
 <script>
 export default {
-  data(){
+  data() {
     return {
-      number0:1
-    }
+      number0: 1,
+      number1: 1
+    };
   }
-}
+};
 </script>
